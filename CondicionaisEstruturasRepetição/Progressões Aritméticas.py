cc549efae48fb5fa62db412d5a7fd95c9132edac
danilo100kl <danilo100kl@hotmail.com>
2018-08-04 23:58:58 -0300

    inserindo mais questões

--- CondicionaisEstruturasRepetição/Progressões Aritméticas.py
+++ CondicionaisEstruturasRepetição/Progressões Aritméticas.py
@@ -1,29 +1,14 @@
-quant = int(input())
-lista = input()
-lista = lista.split(' ')
-lista = list(map(float, lista))
-lista.sort()
-total = 1
-listaNova = []
-if(quant <= 2):
-    print(total)
-else:
-    valor = lista[1] - lista[0]
-    for i in range(quant-1):
-        if(valor != (lista[i+1] - lista[i])):
+import math
+quantidade = int(input())
+entrada = list(map(int,input().split(' ')))
+proximo=abs(entrada[0]-entrada[1])
+contador=1
 
-
-____________________________________________________________
-            if(i == quant-1):
-                valor = lista[i+1] - lista[i]
-                listaNova.append(valor)
-            elif():
-____________________________________________________________
-
-
-    listaNova.sort()
-    for i in range(len(listaNova)-1):
-        if(listaNova[i] != listaNova[i+1]):
-            total += 1
-    print(total)
-
+for i in range(quantidade-1):
+    atual = abs(entrada[i]-entrada[i+1])
+    if(atual!=proximo):
+        contador+=1
+        if(i+2!=len(entrada)):
+            atual = abs(entrada[i+1]-entrada[i+2])
+    proximo=atual
+print(contador)
